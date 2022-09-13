796799d8d4b9fc03bc178a56574ff85b877c2bd2
amitkumar-cloudeq <amit.kumar@cloudeq.com>
2022-09-13 21:06:18 +0530

    all repo code on main

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-resource "github_repository" "example" {
-  name         = "example"
-  description  = "My awesome codebase"
-  has_projects = true
+resource "github_repository" "git_repo" {
+  nam = var.github_repository_description
+  description = var.github_repository_description
+  visibility = var.github_repository_visibility
 }
 
-resource "github_repository_project" "project" {
-  name       = "A Repository Project"
-  repository = "${github_repository.example.name}"
-  body       = "This is a repository project."
+
+output "gitrepo" {
+    value = github_repository.git_repo.name
 }
